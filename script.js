a5a83d6c4b456a1904c31dd0cbebd2ff42e3344a
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-07-27 21:27:03 +0500

    some changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to make the navigation menu toggle on mobile
+function toggleMenu() {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+}
